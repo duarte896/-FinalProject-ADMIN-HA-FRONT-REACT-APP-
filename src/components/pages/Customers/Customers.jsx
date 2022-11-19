16bbf7ea23e04136f43089bb86067ae0f08211f1
Colu17 <collazzi.lucia@gmail.com>
2022-11-18 23:16:41 -0300

    se arregla typo

--- src/components/pages/Customers/Customers.jsx
+++ src/components/pages/Customers/Customers.jsx
@@ -24,7 +24,7 @@ function Users(params) {
         <Sidebar />
         <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
           <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 className="h2">Costumers</h1>
+            <h1 className="h2">Customers</h1>
           </div>
           <div className="table-responsive">
             <table className="table table-striped table-sm">
