2a431b71a9035b817f50baae234d6a8179523968
duarte896 <duarte.manue10@gmail.com>
2022-11-21 16:28:31 -0300

    se elimina cosas inecesarias de sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -62,54 +62,6 @@ function Sidebar() {
             </Link>
           </li>
         </ul>
-
-        <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
-          <span>Saved reports</span>
-          <a className="link-secondary" href="#" aria-label="Add a new report">
-            <span
-              data-feather="plus-circle"
-              className="align-text-bottom"
-            ></span>
-          </a>
-        </h6>
-        <ul className="nav flex-column mb-2">
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <span
-                data-feather="file-text"
-                className="align-text-bottom"
-              ></span>
-              Current month
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <span
-                data-feather="file-text"
-                className="align-text-bottom"
-              ></span>
-              Last quarter
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <span
-                data-feather="file-text"
-                className="align-text-bottom"
-              ></span>
-              Social engagement
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <span
-                data-feather="file-text"
-                className="align-text-bottom"
-              ></span>
-              Year-end sale
-            </a>
-          </li>
-        </ul>
       </div>
     </nav>
   );
